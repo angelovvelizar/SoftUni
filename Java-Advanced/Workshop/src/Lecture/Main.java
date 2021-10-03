c95cf89f00b4aa4ae21cec0c49d0d3ff6ed09f2d
angelovvelizar <88190854+angelovvelizar@users.noreply.github.com>
2021-10-03 19:40:50 +0300

    Added SmartArray and Stack implementations

---
+++ Java-Advanced/Workshop/src/Lecture/Main.java
@@ -0,0 +1,23 @@
+package Lecture;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        SmartArray smartArray = new SmartArray();
+        smartArray.add(13);
+        smartArray.add(42);
+        smartArray.add(69);
+        smartArray.add(69);
+        smartArray.add(69);
+        System.out.println(smartArray.size());
+
+
+
+        List<Integer> numbers = new ArrayList<>();
+
+    }
+}
