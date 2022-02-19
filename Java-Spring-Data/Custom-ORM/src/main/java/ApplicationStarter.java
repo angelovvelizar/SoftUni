17fdc1e5313192e0424e28123b99ce6b355e168c
Velizar <v.angelovtm@gmail.com>
2022-02-19 02:36:08 +0200

    Added custom ORM project

---
+++ Java-Spring-Data/Custom-ORM/src/main/java/ApplicationStarter.java
@@ -0,0 +1,28 @@
+import customORMDemo.entity.Address;
+import customORMDemo.entity.Employee;
+import ormFramework.core.EntityManager;
+import ormFramework.core.EntityManagerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+
+public class ApplicationStarter {
+    public static void main(String[] args) throws SQLException, URISyntaxException, ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
+        EntityManager entityManager = EntityManagerFactory.create(
+                "mysql",
+                "localhost",
+                3306,
+                "root",
+                "",
+                "test_orm",
+                ApplicationStarter.class
+        );
+
+        Employee byId = entityManager.findById(25, Employee.class);
+
+        Address softUniAddress = entityManager.findById(1, Address.class);
+        Address codexioAddress = entityManager.findById(2, Address.class);
+//        Department byId1 = entityManager.findById(30, Department.class);
+    }
+}
